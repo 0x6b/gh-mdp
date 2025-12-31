9c6c761f8381300b12ea2c22164540115b84ff91
kaoru <k@warpnine.io>
2025-12-31 19:05:38 +0900

    refactor(args): simplify clap derive attributes

--- src/args.rs
+++ src/args.rs
@@ -2,17 +2,14 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[clap(about, version)]
+#[derive(Parser)]
+#[command(about, version)]
 pub struct Args {
     /// Markdown file or directory to preview (defaults to index.md or README.md)
-    #[arg()]
     pub file: Option<PathBuf>,
-
     /// Bind address
     #[arg(short, long, default_value = "127.0.0.1")]
     pub bind: String,
-
     /// Don't open browser automatically
     #[arg(long)]
     pub no_open: bool,
